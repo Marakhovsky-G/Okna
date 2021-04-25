1a786e50667080a55a0563ef7fd9f82d85b4fa7d
Georgy <marakhovskij@gmail.com>
2021-04-25 17:03:53 +0300

    Сделал базовые модалки

---
+++ src/js/modules/modals.js
@@ -0,0 +1,50 @@
+
+const modals = () => {
+
+  function modalOpen(modalSelector) {
+    document.querySelector(modalSelector).style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  }
+
+  function modalClose(modalSelector) {
+    document.querySelector(modalSelector).style.display = 'none';
+    document.body.style.overflow = '';
+  }
+
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+          modal = document.querySelector(modalSelector),
+          close = document.querySelector(closeSelector);
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (evt) => {
+        if (evt.target) {
+          evt.preventDefault();
+        }
+
+        modalOpen(modalSelector);
+      });
+    })
+
+    close.addEventListener('click', () => {
+      modalClose(modalSelector);
+    });
+
+    modal.addEventListener('click', (evt) => {
+      if (evt.target === modal) {
+        modalClose(modalSelector);
+      }
+    });
+  }
+
+  function showModalByTime(selector, time) {
+    setTimeout(() => {modalOpen(selector)}, time);
+  }
+
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  // showModalByTime('.popup', 60000);
+};
+
+export default modals;
+
