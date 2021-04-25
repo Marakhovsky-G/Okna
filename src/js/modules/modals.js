e6ea5fb3b32aa68efd3783961e900d6bc1772f4a
Georgy <marakhovskij@gmail.com>
2021-04-25 20:32:30 +0300

    Добавил форму-калькулятор.

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -11,10 +11,11 @@ const modals = () => {
     document.body.style.overflow = '';
   }
 
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
     const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
-          close = document.querySelector(closeSelector);
+          close = document.querySelector(closeSelector),
+          windows = document.querySelectorAll('[data-modal]');
 
     trigger.forEach(item => {
       item.addEventListener('click', (evt) => {
@@ -22,16 +23,26 @@ const modals = () => {
           evt.preventDefault();
         }
 
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
+
         modalOpen(modalSelector);
       });
     });
 
     close.addEventListener('click', () => {
+      windows.forEach(item => {
+        item.style.display = 'none';
+      });
       modalClose(modalSelector);
     });
 
     modal.addEventListener('click', (evt) => {
-      if (evt.target === modal) {
+      if (evt.target === modal && closeClickOverlay) {
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
         modalClose(modalSelector);
       }
     });
@@ -43,6 +54,9 @@ const modals = () => {
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
+  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
   // showModalByTime('.popup', 60000);
 };
 
